024ffb31311a9d6fb5a971a72c0d6ae4d630014f
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-02-05 18:49:24 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {'path': 'home', title: 'Home', component: HomeComponent},
+    {'path': 'about', title: 'About', component: AboutComponent},
+    {'path': 'contact', title: 'Contact', component: ContactComponent},
+    {'path': '', redirectTo: '/home', pathMatch: 'full'},
+    {'path': '**', component: PageNotFoundComponent}
+
+];
